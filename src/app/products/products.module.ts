7b0b7ec918353473f5f1c5ca181e59a70a25394b
{AngelCortesM} <joseangelcortesmontoya@gmail.com>
2024-12-30 17:01:52 -0500

    Agregar formulario reactivo en la página de producto y directiva personalizada para manejo de errores

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ProductPageComponent
-  ],
+  declarations: [ProductPageComponent],
   imports: [
     CommonModule,
-    ProductsRoutingModule
-  ]
+    ProductsRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
-export class ProductsModule { }
+export class ProductsModule {}
