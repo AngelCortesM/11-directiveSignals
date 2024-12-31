97e3a8268285ba05e685e4c1bc124d0635a40aef
AngelCortesM <joseangelcortesmontoya@gmail.com>
2024-12-31 10:21:06 -0500

    Agregar componente SignalsLayout y su enrutamiento

--- src/app/signals/signals-routing.module.ts
+++ src/app/signals/signals-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SignalsLayoutComponent,
+    // children: [
+    //   {
+    //     path: 'signals',
+    //     loadChildren: () =>
+    //       import('./signals.module').then((m) => m.SignalsModule),
+    //   },
+    // ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
