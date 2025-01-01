71e08824402743da7b3c8836419bcc2261a5bf06
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-01-01 13:37:34 -0500

    Agregar página de información del usuario con carga dinámica y manejo de errores

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-info-page',
   standalone: false,
-  
   templateUrl: './user-info-page.component.html',
-  styleUrl: './user-info-page.component.css'
+  styleUrl: './user-info-page.component.css',
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  private readonly userService = inject(UsersServiceService);
+  public userId = signal(1);
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
 
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+  loadUser(id: number) {
+    if (id <= 0) {
+      return;
+    }
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+    this.userService.getUserById(id).subscribe(
+      (user) => {
+        this.currentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      () => {
+        this.currentUser.set(undefined);
+        this.userWasFound.set(false);
+      }
+    );
+  }
 }
