fe5c9036f13b7e229604c28b18fb63ff4c11b25e
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-01-02 11:41:12 -0500

    Agregar página de propiedades con mutaciones de usuario y manejo de estado

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { UsersServiceService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -13,6 +13,12 @@ export class UserInfoPageComponent implements OnInit {
   public userId = signal(1);
   public currentUser = signal<User | undefined>(undefined);
   public userWasFound = signal(true);
+  public fullName = computed<string>(() => {
+    if (!this.currentUser()) {
+      return 'Usuario no encontrado';
+    }
+    return `${this.currentUser()!.first_name} ${this.currentUser()?.last_name}`;
+  });
 
   ngOnInit(): void {
     this.loadUser(this.userId());
@@ -23,15 +29,15 @@ export class UserInfoPageComponent implements OnInit {
     }
     this.userId.set(id);
     this.currentUser.set(undefined);
-    this.userService.getUserById(id).subscribe(
-      (user) => {
+    this.userService.getUserById(id).subscribe({
+      next: (user) => {
         this.currentUser.set(user);
         this.userWasFound.set(true);
       },
-      () => {
+      error: () => {
         this.currentUser.set(undefined);
         this.userWasFound.set(false);
-      }
-    );
+      },
+    });
   }
 }
